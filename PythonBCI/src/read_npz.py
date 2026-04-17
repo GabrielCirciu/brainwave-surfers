5a8569da742d9325388f0b94382876b385976d93
Mykyta Taranov <131954932+versroot@users.noreply.github.com>
2026-04-17 11:57:14 +0200

    chore: More tests

--- PythonBCI/src/read_npz.py
+++ PythonBCI/src/read_npz.py
@@ -1,7 +1,7 @@
 import sys
 import numpy as np
 from numpy import load
-document = "PythonBCI\\data\\raw\\output_data_5.npz"
+document = "PythonBCI\\data\\raw\\output_data_6.npz"
 #document=input() 
 np.set_printoptions(threshold=sys.maxsize)
 data = load(document)
@@ -9,7 +9,7 @@ print(data)
 lst = data.files
 print(lst)
 epoch=input()
-print(data[lst[0]][int(epoch)][0])
+print(data[lst[1]][int(epoch)])
 #for item in lst:
 #    print(item)
 #    print(data[item])
