303a3e09092d44ef303fb330abaa1c1237a594d1
Mykyta Taranov <131954932+versroot@users.noreply.github.com>
2026-04-21 09:55:44 +0200

    chore: Small fixes

--- PythonBCI/src/read_npz.py
+++ PythonBCI/src/read_npz.py
@@ -4,7 +4,8 @@ from numpy import load
 
 np.set_printoptions(threshold=sys.maxsize)
 
-the_file = input("Enter the file name: ")
+the_file = "output_data_20260420_1150.npz"
+# the_file = input("Enter the file name: ")
 document = "PythonBCI\\data\\raw\\" + the_file
 data = load(document)
 lst = data.files
@@ -12,18 +13,16 @@ lst = data.files
 print('\nData: ', data)
 print('Data keys: ', lst)
 
-print(f'\Epoch 0 data')
-
 print('\nEEG Data')
 print('Shape: ', data[lst[0]].shape)
-print('Signal 0 data: ', data[lst[0]][0][0])
+print('Epoch 0 -> Signal 0 data: ', data[lst[0]][0][0])
 
 print('\nAUX Data')
 print('Shape: ', data[lst[1]].shape)
-print('Signal 0 data: ', data[lst[1]][0][0])
+print('Epoch 0 -> Signal 0 data: ', data[lst[1]][0][0])
 
 print('\nLabels Data')
 print('Shape: ', data[lst[2]].shape)
-print('Trial 0 label: ', data[lst[2]][0])
+print('Epoch 0 label: ', data[lst[2]][0])
 
 print(f'\n')
